a26812a6c26aa555234d028f84510c4edf4814ef
Sambo Chea <sombochea@cubetiqs.com>
2020-07-11 21:01:38 +0700

    Init wsclient and code tests with rsocket client

---
+++ src/test/kotlin/com/cubetiqs/wsclient/RSocketClientApplicationTests.kt
@@ -0,0 +1,23 @@
+package com.cubetiqs.wsclient
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import reactor.core.publisher.Flux
+
+@SpringBootTest
+class RSocketClientApplicationTests @Autowired constructor(
+		private val stockClient: StockClient
+) {
+	@Test
+	fun shouldRetrieveStockPricesFromTheService() {
+		val prices: Flux<StockPrice> = stockClient.priceFor("USD")
+
+		Assertions.assertNotNull(prices)
+		val fivePrices = prices.take(5)
+		Assertions.assertEquals(5, fivePrices.count().block())
+		Assertions.assertEquals("USD", fivePrices.blockFirst()?.symbol)
+	}
+
+}
