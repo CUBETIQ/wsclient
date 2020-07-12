aea0ee5b224fdf173e5b1dc2ed14670b564615c8
Sambo Chea <sombochea@cubetiqs.com>
2020-07-12 12:33:48 +0700

    Setup rsocket stock client rest api

--- src/main/kotlin/com/cubetiqs/wsclient/ClientConfig.kt
+++ src/main/kotlin/com/cubetiqs/wsclient/ClientConfig.kt
@@ -3,6 +3,7 @@ package com.cubetiqs.wsclient
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.messaging.rsocket.RSocketRequester
 import org.springframework.web.reactive.function.client.WebClient
 
@@ -15,12 +16,14 @@ class ClientConfig {
     }
 
     @Bean
-    fun webClientStockClient(webClient: WebClient): WebClientStockClient {
+    @Profile("sse")
+    fun webClientStockClient(webClient: WebClient): StockClient {
         return WebClientStockClient(webClient)
     }
 
     @Bean
-    fun stockClient(requester: RSocketRequester): RSocketStockClient {
+    @Profile("rsocket")
+    fun stockClient(requester: RSocketRequester): StockClient {
         return RSocketStockClient(requester)
     }
 
--- src/main/kotlin/com/cubetiqs/wsclient/WsClientApplication.kt
+++ src/main/kotlin/com/cubetiqs/wsclient/WsClientApplication.kt
@@ -1,6 +1,27 @@
 package com.cubetiqs.wsclient
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
 
 @SpringBootApplication
 class WsClientApplication
+
+fun main(args: Array<String>) {
+    runApplication<WsClientApplication>(*args)
+}
+
+@RestController
+class RestController @Autowired constructor(
+        private val stockClient: StockClient
+) {
+    @GetMapping("/{symbol}", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun getStockPrices(@PathVariable symbol: String): Flux<StockPrice> {
+        return stockClient.priceFor(symbol)
+    }
+}
